13cc847565a7c478ef7c13937b885c41c71b4801
Justin Ng <jng@slack-corp.com>
2020-03-10 23:34:14 -0600

    Add edit and delete capabilities on Invite

--- invites.py
+++ invites.py
@@ -92,9 +92,90 @@ def create_invites(jwt):
     return jsonify(response)
 
 
+@invites.route('/api/v1/invites/<int:id>', methods=['PATCH'])
+@cross_origin()
+# @requires_auth(permission='create:invites')
+@requires_auth()
+def edit_invites(jwt, id):
+    """
+    Edit invite
+
+    - Event must already exist in the database
+    - Use Guests API to modify guests on invite
+    - It is not recommended to use this endpoint to regenerate invite codes,
+      instead, use /api/v1/invites/regenerate
+
+    PATCH /api/v1/invites/<id>
+    {
+        "accepted": False,
+        "event_id": 2
+    }
+
+    Keyword arguments:
+    id -- the id of the invite
+    """
+    payload = request.get_json()
+    if payload is None:
+        abort(400)
+    invite = Invite.query.get(id)
+    if invite is None:
+        abort(404)
+    try:
+        for key in payload.keys():
+            if key == 'invite_code':
+                raise
+            setattr(invite, key, payload.get(key))
+    except Exception:
+        current_app.logger.info(sys.exc_info())
+        abort(400)
+    try:
+        invite.update()
+        response = {
+            'success': True,
+            'invite': invite.to_dict()
+        }
+    except Exception:
+        rollback()
+        current_app.logger.info(sys.exc_info())
+        abort(422)
+    finally:
+        close()
+    return jsonify(response)
+
+
+@invites.route('/api/v1/invites/<int:id>', methods=['DELETE'])
+@cross_origin()
+# @requires_auth(permission='create:invites')
+@requires_auth()
+def delete_invites(jwt, id):
+    """
+    Delete invite
+
+    DELETE /api/v1/invites/<id>
+
+    Keyword arguments:
+    id -- the id of the invite
+    """
+    invite = Invite.query.get(id)
+    if invite is None:
+        abort(404)
+    try:
+        invite.delete()
+        response = {
+            'success': True
+        }
+    except Exception:
+        rollback()
+        current_app.logger.info(sys.exc_info())
+        abort(422)
+    finally:
+        close()
+    return jsonify(response)
+
+
 @invites.route('/api/v1/invites/verify', methods=['GET'])
 @cross_origin()
-# @requires_auth(permission='create:guests')
+# @requires_auth(permission='read:invites')
 @requires_auth()
 def verify_invites(jwt):
     """
